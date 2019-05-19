09e2b5e983bd3dc4bf61e4fe39210e25e696fecb
Manuel Núñez <manuel.nunez@hawkore.com>
2019-05-19 10:29:43 +0200

    FIX - avoid to remove IGNITE_* user attributes
    - Useful for ignite web agent (IGNITE_CLUSTER_NAME)
    - "IGNITE_*" will take same consideration as "org.apache.ignite.*"

--- modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/top/GridTopologyCommandHandler.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/rest/handlers/top/GridTopologyCommandHandler.java
@@ -298,7 +298,7 @@ public class GridTopologyCommandHandler extends GridRestCommandHandlerAdapter {
             for (Iterator<Map.Entry<String, Object>> i = attrs.entrySet().iterator(); i.hasNext();) {
                 Map.Entry<String, Object> e = i.next();
 
-                if (!e.getKey().startsWith("org.apache.ignite.") && !e.getKey().startsWith("plugins.") &&
+                if (!e.getKey().startsWith("IGNITE_") && !e.getKey().startsWith("org.apache.ignite.") && !e.getKey().startsWith("plugins.") &&
                     System.getProperty(e.getKey()) == null) {
                     i.remove();
 
